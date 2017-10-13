943fe6ea8087ed16f0d768ab6ffd3d800927fd31
A R Rowley <tonyrowley2000@gmail.com>
2017-10-13 17:33:18 +0100

    MDN Node Tutorial
    This after Chapter 4

---
+++ app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
+
+var app = express();
+
+//Setup mongoose application
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb://tonyrowley2000:Matiz-Mlab-Local_library-2017@ds119685.mlab.com:19685/local_library'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+/*
+var mongoDB = mongodb://tonyrowley2000:Matiz-Mlab-Local_library-2017@ds119685.mlab.com:19685/local_library;
+
+mongoose.connect(mongoDB, {
+    unseMongoClient = true
+});
+*/
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/catalog', catalog);  // Add catalog routes to middleware chain.
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
